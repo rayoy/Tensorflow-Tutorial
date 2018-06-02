d3c074e5352e537b77acf0d6c3adef5bbc5db4c8
wanglei60 <wanglei60@baidu.com>
2018-06-02 17:32:18 +0800

    add other tf.constant tf.Session test by v1.8.

--- tutorial-contents/201_session.py
+++ tutorial-contents/201_session.py
@@ -3,24 +3,57 @@ Know more, visit my Python tutorial page: https://morvanzhou.github.io/tutorials
 My Youtube Channel: https://www.youtube.com/user/MorvanZhou
 
 Dependencies:
-tensorflow: 1.1.0
+tensorflow: 1.8.0
 """
 import tensorflow as tf
 
-m1 = tf.constant([[2, 2]])
-m2 = tf.constant([[3],
-                  [3]])
-dot_operation = tf.matmul(m1, m2)
 
-print(dot_operation)  # wrong! no result
+def main(argv):
+    m1 = tf.constant([[2, 2]])
+    m2 = tf.constant([[3],
+                      [3]])
 
-# method1 use session
-sess = tf.Session()
-result = sess.run(dot_operation)
-print(result)
-sess.close()
+    # m2的另外一种写法，两行一列，填充数字 3 的矩阵
+    m3 = tf.constant(3, shape=[2, 1])
 
-# method2 use session
-with tf.Session() as sess:
-    result_ = sess.run(dot_operation)
-    print(result_)
+    dot_operation = tf.matmul(m1, m2)
+    dot_operation_ = tf.matmul(m1, m3)
+
+    print(dot_operation)  # wrong! no result
+
+    # method1 Using the `close()` method.
+    sess = tf.Session()
+    result = sess.run(dot_operation)
+    print(result)
+    sess.close()
+
+    # method2 Using the context manager.
+    with tf.Session() as sess:
+        # result_ = sess.run(dot_operation)
+        # print(result_)
+        result_ = sess.run(dot_operation_)
+        print(result_)
+
+    # tf.constant
+    # Constant 1-D Tensor populated with value list.
+    # [1 2 3 4 5 6 7]
+    tensor = tf.constant([1, 2, 3, 4, 5, 6, 7])
+
+    print(tensor)
+
+    # Constant 2-D tensor populated with scalar value -1.
+    # [[-1. - 1. - 1.]
+    # [-1. - 1. - 1.]]
+    tensor = tf.constant(-1.0, shape=[2, 3])
+
+    print(tensor)
+
+    # Lists available devices in this session.
+    devices = sess.list_devices()
+    for d in devices:
+        print(d.name)
+
+
+if __name__ == '__main__':
+    tf.logging.set_verbosity(tf.logging.INFO)
+    tf.app.run(main)
